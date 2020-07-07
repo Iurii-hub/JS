ce49003117cb59b7cc807cfc3f8d3b794d95c36d
IuriiLavreniuk <st.play31@gmail.com>
2020-07-07 23:44:59 +0300

    HW3 new add

--- JS HW3/script.js
+++ JS HW3/script.js
@@ -1,4 +1,4 @@
-console.log("test");
+// console.log("test");
 
 // -- створити об'єкт (не меньше 5ти властивостей) який описує
 // - собаку
@@ -7,34 +7,78 @@ console.log("test");
 // - квартиру
 // - книгу
 
+let dog010 = {name: "Juk", age: 2, eyes: "brown", legs: 4, tail: 1};
+let human011 = {name: "Tim", age: 23, eyes: "green", children: false, education: "bachelor"};
+let car012 = {brand: "MAZDA", year: 2011, engine: "2.0L", fuel: "petrol", gearbox: "manual"};
+let flat013 = {adress: "Independence str, 12/321", floor: 12, area: "78 sq.m", room: 3, balcony: true};
+let book014 = {name: "Star kings", author: "E. Hamilton", genre: "sci-fi", year: 1947, hero: "John Gordon"};
+
+
 // -- Створити масив та вивести його в консоль:
 // - з 5 собак
 // - 3 5 людей
 // - з 5 автомобілів
 
-// -- створити об'єкт (не меньше 5ти властивостей) який описує, одна з властивостей обов'язково повинна бути об'єктом,ще одна - масивом
+let dog022 = [
+    {name: "Duk", breed: "shepherd", age: 12, status: true, body: "grey"},
+    {name: "Tuk", breed: "labrador", age: 2, status: true, body: "white"},
+    {name: "Muk", breed: "terier", age: 7, status: false, body: "brown"},
+    {name: "Ruk", breed: "shepherd", age: 9, status: false, body: "dark gray"},
+    {name: "Kuk", breed: "boxer", age: 14, status: true, body: "black"}
+];
+console.log(dog022);
+
+let human031 = [
+    {name: "Kim", age: 31, eyes: "green", children: true, education: "bachelor"},
+    {name: "Serg", age: 21, eyes: "gray", children: false, education: "shool"},
+    {name: "Jim", age: 44, eyes: "brown", children: true, education: "phd"},
+    {name: "Bim", age: 33, eyes: "blue", children: true, education: "master"},
+    {name: "Zim", age: 7, eyes: "dark blue", children: false, education: "no education"},
+];
+console.log(human031);
+// console.log(human031[3].age);
+
+let car041 = [
+    {brand: "Suzuki", year: 2016, engine: "2.5L", fuel: "petrol", gearbox: "automatic"},
+    {brand: "VW", year: 1999, engine: "1.8L", fuel: "diesel", gearbox: "manual"},
+    {brand: "BMW", year: 1988, engine: "3.0L", fuel: "diesel", gearbox: "automatic"},
+    {brand: "MAZDA", year: 2019, engine: "2.0L", fuel: "petrol", gearbox: "automatic"},
+    {brand: "Honda", year: 2003, engine: "1.6L", fuel: "petrol", gearbox: "manual"},
+];
+console.log(car041);
+
+
+
+// -- створити об'єкт (не меньше 5ти властивостей) який описує, одна з властивостей обов'язково повинна бути об'єктом,
+// ще одна - масивом
 // - будинок
 // - водій
 // - іграшку
 // - стіл
 // - сумка
 
+let house060 = {adress: ["Petlury", 21, 44], floor: 2, area: "98 sq.m", room: 5, balcony: true};
+let driver061 = {name: "Toni", age: "53", status: false, skill: ["car", "bus", "truck"], license: true};
+let toy062 = {name: "T-rex"; type: "animal"; color: "green", price: "5$", ability: ["move", "sound"]};
+let table063 = {type: "office", material: ["steel", "plywood"], color: "silver", weight: "27kg", price: "43$"};
+let bag064 = {type: "travel"; material: ["cotton", "rubber"], color: "black", weight: "3kg", price: "26$"};
+
 
 
 // Дан массив:
-// let users = [
-// 				{name: 'vasya', age: 31, status: false},
-// 				{name: 'petya', age: 30, status: true},
-// 				{name: 'kolya', age: 29, status: true},
-// 				{name: 'olya', age: 28, status: false},
-// 				{name: 'max', age: 30, status: true},
-// 				{name: 'anya', age: 31, status: false},
-// 				{name: 'oleg', age: 28, status: false},
-// 				{name: 'andrey', age: 29, status: true},
-// 				{name: 'masha', age: 30, status: true},
-// 				{name: 'olya', age: 31, status: false},
-// 				{name: 'max', age: 31, status: true}
-// 			];
+let users = [
+				{name: 'vasya', age: 31, status: false},
+				{name: 'petya', age: 30, status: true},
+				{name: 'kolya', age: 29, status: true},
+				{name: 'olya', age: 28, status: false},
+				{name: 'max', age: 30, status: true},
+				{name: 'anya', age: 31, status: false},
+				{name: 'oleg', age: 28, status: false},
+				{name: 'andrey', age: 29, status: true},
+				{name: 'masha', age: 30, status: true},
+				{name: 'olya', age: 31, status: false},
+				{name: 'max', age: 31, status: true}
+			];
 // - звернутися в відповідну ячейку масиву і відповідний параметр об'єкта і вивести в консольх наступне
 // - статус Андрія
 // - статус Максима
