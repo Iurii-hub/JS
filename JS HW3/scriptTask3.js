214b0fe8329403c3387376e3e85904b536b4e8cf
Iurii-Hub <st.play31@gmail.com>
2020-07-10 17:01:30 +0300

    HW3 add new

--- JS HW3/scriptTask3.js
+++ JS HW3/scriptTask3.js
@@ -176,90 +176,206 @@ console.log(str174);
 
 
 // - взять json из задания 11 и превратить их обратно в объекты.
+
+let parse166 = JSON.parse(str166);
+console.log(parse166);
+let parse168 = JSON.parse(str168);
+console.log(parse168);
+let parse170 = JSON.parse(str170);
+console.log(parse170);
+let parse172 = JSON.parse(str172);
+console.log(parse172);
+let parse174 = JSON.parse(str174);
+console.log(parse174);
+
+
 // - взять массив из задания 5,в цикле перебрать его объекты превратив их в json .
+
+for (let cars0821 = 0; cars0821 < arrCars082.length; cars0821++) {
+    let strcars0821 = JSON.stringify(arrCars082[cars0821]);
+    console.log(strcars0821);
+}
+
+
 // - взять массив из задания 6,в цикле перебрать его объекты превратив их в json .
+
+for (let cities0981 = 0; cities0981 < arrCities098.length; cities0981++) {
+    let strcities0981 = JSON.stringify(arrCities098[cities0981]);
+    console.log(strcities0981);
+}
+
+
 // - взять массив из задания 7,в цикле перебрать его объекты превратив их в json и сразу скоприовать в новый массив.
-// - Создать массив пользователей. У каждого пользователя обязательно должено быть поле skills которое является массивом. Проитерировать массив пользователей и в каждом пользователе проитерировать его массив skills
-// - Создать массив пользователей. У каждого пользователя обязательно должено быть поле skills которое является массивом. Проитерировать массив пользователей и в каждом пользователе проитерировать его массив skills. Скопировать все skills всех пользователей в отедльный массив
+
+let arrCars210 = [];
+for (let cars1071 = 0; cars1071 < arrCars107.length; cars1071++) {
+    arrCars210[cars1071] = JSON.stringify(arrCars107[cars1071]);
+    console.log(arrCars210[cars1071]);  
+}
+
+
+// - Создать массив пользователей. У каждого пользователя обязательно должено быть поле skills которое является массивом. 
+// Проитерировать массив пользователей и в каждом пользователе проитерировать его массив skills
+
+let users = [
+    {name: 'vasya', age: 31, status: false, skills: ["java", "js", "python"]},
+    {name: 'petya', age: 30, status: true, skills: ["js", "node.js"]},
+    {name: 'kolya', age: 29, status: true , skills: ["java", "spring"]},
+    {name: 'olya', age: 28, status: false, skills: ["html", "css"]},
+    {name: 'max', age: 30, status: true, skills: ["php", "java"]}
+];
+
+for (let users228 of users) {
+    for (let i = 0; i < users228.skills.length; i++) {
+        console.log(users228.skills[i]);
+    }
+}
+
+// - Создать массив пользователей. У каждого пользователя обязательно должено быть поле skills которое является массивом. 
+// Проитерировать массив пользователей и в каждом пользователе проитерировать его массив skills. Скопировать все skills всех пользователей в отедльный массив
+
+let usersNew =[];
+for (let users238 of users) {
+    // console.log(users238);
+    for (let i = 0; i < users238.skills.length; i++) {
+        // console.log(users238.skills[i]);
+        usersNew.push(users238.skills[i]);
+        console.log(usersNew[i]); //!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    }
+} 
+
 
 // - За допомогою 2х циклів циклів проітеррувати  даний масив і масив кожного об'єкта.
-//  let users = [{name: 'vasya', age: 31, status: false, skills: ['java', 'js']}, {name: 'petya', age: 30, status: true, skills: ['java', 'js', 'html']}, {name: 'kolya', age: 29, status: true, skills: ['mysql', ',mongo']}, {name: 'olya', age: 28, status: false, skills: ['java', 'js']}, {name: 'max', age: 30, status: true, skills: ['mysql', ',mongo']}];
+ 
+let users2 = [
+    {name: 'vasya', age: 31, status: false, skills: ['java', 'js']}, 
+    {name: 'petya', age: 30, status: true, skills: ['java', 'js', 'html']}, 
+    {name: 'kolya', age: 29, status: true, skills: ['mysql', ',mongo']}, 
+    {name: 'olya', age: 28, status: false, skills: ['java', 'js']}, 
+    {name: 'max', age: 30, status: true, skills: ['mysql', ',mongo']}
+];
 
+for (let users254 of users2) {
+    console.log(users254);
+    for (let i = 0; i < users254.skills.length; i++) {
+        console.log(users254.skills[i]);
+    }
+}
 
 
 // - З масиву users за допомогою циклу витягнути адреси користувачів і записати (скопіювати) їх в інший порожній масив.
-// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement. Всі данні в одному блоці.
-// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам (div>div*4)
-// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам , блок з адресою зробити окремим блоком, з блоками для кожної властивості
 
-//             			let users = [{
-//             				name: 'vasya',
-//             				age: 31,
-//             				status: false,
-//             				address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
-//             			}, {
-//             				name: 'petya',
-//             				age: 30,
-//             				status: true,
-//             				address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
-//             			}, {
-//             				name: 'kolya',
-//             				age: 29,
-//             				status: true,
-//             				address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
-//             			}, {
-//             				name: 'olya',
-//             				age: 28,
-//             				status: false,
-//             				address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
-//             			}, {
-//             				name: 'max',
-//             				age: 30,
-//             				status: true,
-//             				address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
-//             			}, {
-//             				name: 'anya',
-//             				age: 31,
-//             				status: false,
-//             				address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
-//             			}, {
-//             				name: 'oleg',
-//             				age: 28,
-//             				status: false,
-//             				address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
-//             			}, {
-//             				name: 'andrey',
-//             				age: 29,
-//             				status: true,
-//             				address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
-//             			}, {
-//             				name: 'masha',
-//             				age: 30,
-//             				status: true,
-//             				address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
-//             			}, {
-//             				name: 'olya',
-//             				age: 31,
-//             				status: false,
-//             				address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
-//             			}, {
-//             				name: 'max',
-//             				age: 31,
-//             				status: true,
-//             				address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
-//             			}];
+let users3 = [{
+    name: 'vasya',
+    age: 31,
+    status: false,
+    address: {city: 'Lviv', country: 'Ukraine', street: 'Shevchenko', houseNumber: 1}
+}, {
+    name: 'petya',
+    age: 30,
+    status: true,
+    address: {city: 'New York', country: 'USA', street: 'East str', houseNumber: 21}
+}, {
+    name: 'kolya',
+    age: 29,
+    status: true,
+    address: {city: 'Budapest', country: 'Hungary', street: 'Kuraku', houseNumber: 78}
+}, {
+    name: 'olya',
+    age: 28,
+    status: false,
+    address: {city: 'Prague', country: 'Czech', street: 'Paster', houseNumber: 56}
+}, {
+    name: 'max',
+    age: 30,
+    status: true,
+    address: {city: 'Istanbul', country: 'Turkey', street: 'Mikar', houseNumber: 39}
+}, {
+    name: 'anya',
+    age: 31,
+    status: false,
+    address: {city: 'Rio', country: 'Brasil', street: 'Ronaldi', houseNumber: 5}
+}, {
+    name: 'oleg',
+    age: 28,
+    status: false,
+    address: {city: 'Montreal', country: 'Canada', street: 'Acusto', houseNumber: 90}
+}, {
+    name: 'andrey',
+    age: 29,
+    status: true,
+    address: {city: 'Quebeck', country: 'Canada', street: 'Binaro', houseNumber: 33}
+}, {
+    name: 'masha',
+    age: 30,
+    status: true,
+    address: {city: 'Moscow', country: 'Russia', street: 'Gogolia', houseNumber: 1}
+}, {
+    name: 'olya',
+    age: 31,
+    status: false,
+    address: {city: 'Portland', country: 'USA', street: 'Forest str', houseNumber: 4}
+}, {
+    name: 'max',
+    age: 31,
+    status: true,
+    address: {city: 'Cairo', country: 'Egypt', street: 'Seashore', houseNumber: 45}
+}];
+
+
+
+
+
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement. Всі данні в одному блоці.
+
+
+
+
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам (div>div*4)
+
+for (let i = 0; i < users3.length; i++) {
+    let div3 = document.createElement("div");
+
+
+    document.body.appendChild(div3);
+}
+
+
+// - За допомоги циклу проітерувати  масив users, записати кожного юзера в сівй блок за допомоги document.createElement, розділивши всі властивості по своїм блокам , 
+// блок з адресою зробити окремим блоком, з блоками для кожної властивості
+
+   
+
+
 
 
 // - Дано 2 масиви з рівною кількістю об'єктів.
 // Масиви:
-//             let usersWithId = [{id: 1, name: 'vasya', age: 31, status: false}, {id: 2, name: 'petya', age: 30, status: true}, {id: 3, name: 'kolya', age: 29, status: true}, {id: 4, name: 'olya', age: 28, status: false},];
-//             let citiesWithId = [{user_id: 3, country: 'USA', city: 'Portland'}, {user_id: 1, country: 'Ukraine', city: 'Ternopil'}, {user_id: 2, country: 'Poland', city: 'Krakow'}, {user_id: 4, country: 'USA', city: 'Miami'},];
+            let usersWithId = [
+                {id: 1, name: 'vasya', age: 31, status: false},
+                {id: 2, name: 'petya', age: 30, status: true},
+                {id: 3, name: 'kolya', age: 29, status: true},
+                {id: 4, name: 'olya', age: 28, status: false},
+            ];
+
+            let citiesWithId = [{user_id: 3, country: 'USA', city: 'Portland'},
+            {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+            {user_id: 2, country: 'Poland', city: 'Krakow'},
+            {user_id: 4, country: 'USA', city: 'Miami'},
+            ];
+
 // З'єднати в один об'єкт користувача та місто з відповідними "id" та "user_id" .
+
+
+
+
 // Записати цей об'єкт в новий масив
+
+
+
+
 // Частковий приклад реультату:
 // let usersWithCities = [{id: 1, name: 'vasya', age: 31, status: false, address: {user_id: 1, country: 'Ukraine', city: 'Ternopil'}}....]
-
-
 
 
 
