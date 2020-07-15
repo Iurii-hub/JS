064e3f8b9e8796b5522a31956165f42f81f7deb9
Iurii-Hub <st.play31@gmail.com>
2020-07-15 10:09:23 +0300

    HW4 add

--- JS HW4/script4.js
+++ JS HW4/script4.js
@@ -300,47 +300,23 @@ getCarList(arrCars, "car-list");
 
 // Для кожного автомобіля створити свій блок, та додати його в елемент, індентифікатор якого ви отримали. Всі властивості авто в обному блоці
 
-function getCarId (count, typeTag) {
-  for (let i = 0; i < count; i++) {
-    const tag = document.createElement(typeTag);
-    tag.id = i;
-    document.body.appendChild(tag);
-  }
-}
-
-
-function getCars (cars) {
-  const block = document.createElement("div");
-  const model = document.createElement("h2");
-  model.innerText = cars.model;
-  const year = document.createElement("h3");
-  year.innerText = cars.year;
-  const hp = document.createElement("p");
-  hp.innerText = cars.hp;
-  const color = document.createElement("p");
-  color.innerText = cars.color
-
-  block.appendChild(model);
-  block.appendChild(year);
-  block.appendChild(hp);
-  block.appendChild(color);
-
-  return (block);
-}
-
-function getCarInBlock(arrayCars) {
-  const listCars = document.querySelectorAll("[id]");
-  console.log(listCars);
-  for (const divElement of listCars) {
-    const cars = arrayCars.find((cars) => cars.id === divElement.id);
-    if (cars) {
-      divElement.appendChild(getCars(cars));
+function getCarId(carArr, idCar) {
+  for (const i of carArr) {
+    for (const j in i) {
+      carProperties = `${j} : ${i[j]}`;
+      const divProprerties = document.createElement("div");
+      const divCar = document.createElement("div");
+      const idCar = document.createElement("idCar");
+      
+      divProprerties.innerText = carProperties;
+      document.body.append(idCar);
+      idCar.append(divCar);
+      divCar.append(carProperties);
+      document.write("----------------------");
     }
   }
 }
-
-getCarId(10, "div");
-getCarInBlock(arrCars);
+getCarId(arrCars);
 
 
 // - приймає масив автомобілів (можна взяти з попередніх дз ),та  індентифікатор елемнту в який потрібно додати список цих автомобілів.
@@ -351,10 +327,31 @@ getCarInBlock(arrCars);
 // **- функція приймає 2 масиви з рівною кількістю об'єктів та з'єднює в один об'єкт користувача та місто з відповідними "id" та "user_id",
 // та повертає масив цих з'єднаних об'єктів.
 // Приклад масивів:
-//             let usersWithId = [{id: 1, name: 'vasya', age: 31, status: false}, {id: 2, name: 'petya', age: 30, status: true}, {id: 3, name: 'kolya', age: 29, status: true}, {id: 4, name: 'olya', age: 28, status: false},];
-//             let citiesWithId = [{user_id: 3, country: 'USA', city: 'Portland'}, {user_id: 1, country: 'Ukraine', city: 'Ternopil'}, {user_id: 2, country: 'Poland', city: 'Krakow'}, {user_id: 4, country: 'USA', city: 'Miami'},];
+            let usersWithId = [
+              {id: 1, name: 'vasya', age: 31, status: false},
+              {id: 2, name: 'petya', age: 30, status: true},
+              {id: 3, name: 'kolya', age: 29, status: true},
+              {id: 4, name: 'olya', age: 28, status: false},
+            ];
+            let citiesWithId = [
+              {user_id: 3, country: 'USA', city: 'Portland'}, 
+              {user_id: 1, country: 'Ukraine', city: 'Ternopil'}, 
+              {user_id: 2, country: 'Poland', city: 'Krakow'}, 
+              {user_id: 4, country: 'USA', city: 'Miami'},
+            ];
 // Частковий приклад реультату:
 
+function user_city(usersWithId, citiesWithId){
+	for (i of usersWithId){
+		for (j of citiesWithId){
+			if (i.id === j.user_id){
+				i.adress = j;
+			}
+		}
+	}
+	return usersWithId;
+}
+console.log(user_city(usersWithId, citiesWithId));
 
 
 // ***- беремо завдання з правилами з укроку №3 :
@@ -363,42 +360,59 @@ getCarInBlock(arrCars);
 // При цому в блоці, номер правила записати в свій блок, текст правила записати в свій окремий блок.
 // Результатом відпрацювання скріпта повинна бути структура яка міститься в блоці wrap файла rule.html
 
-// 			let rules = [
-// 				{
-// 					title: 'Первое правило Бойцовского клуба.',
-// 					body: 'Никому не рассказывать о Бойцовском клубе.'
-// 				},
-// 				{
-// 					title: 'Второе правило Бойцовского клуба.',
-// 					body: 'Никогда никому не рассказывать о Бойцовском клубе.'
-// 				},
-// 				{
-// 					title: 'Третье правило Бойцовского клуба.',
-// 					body: 'В схватке участвуют только двое.'
-// 				},
-// 				{
-// 					title: 'Четвертое правило Бойцовского клуба.',
-// 					body: 'Не более одного поединка за один раз.'
-// 				},
-// 				{
-// 					title: 'Пятое правило Бойцовского клуба.',
-// 					body: 'Бойцы сражаются без обуви и голые по пояс.'
-// 				},
-// 				{
-// 					title: 'Шестое правило Бойцовского клуба.',
-// 					body: 'Поединок продолжается столько, сколько потребуется.'
-// 				},
-// 				{
-// 					title: 'Седьмое правило Бойцовского клуба.',
-// 					body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
-// 				},
-// 				{
-// 					title: 'Восьмое и последнее правило Бойцовского клуба.',
-// 					body: 'Новичок обязан принять бой.'
-// 				},
+			let rules = [
+				{
+					title: 'Первое правило Бойцовского клуба.',
+					body: 'Никому не рассказывать о Бойцовском клубе.'
+				},
+				{
+					title: 'Второе правило Бойцовского клуба.',
+					body: 'Никогда никому не рассказывать о Бойцовском клубе.'
+				},
+				{
+					title: 'Третье правило Бойцовского клуба.',
+					body: 'В схватке участвуют только двое.'
+				},
+				{
+					title: 'Четвертое правило Бойцовского клуба.',
+					body: 'Не более одного поединка за один раз.'
+				},
+				{
+					title: 'Пятое правило Бойцовского клуба.',
+					body: 'Бойцы сражаются без обуви и голые по пояс.'
+				},
+				{
+					title: 'Шестое правило Бойцовского клуба.',
+					body: 'Поединок продолжается столько, сколько потребуется.'
+				},
+				{
+					title: 'Седьмое правило Бойцовского клуба.',
+					body: 'Если противник потерял сознание или делает вид, что потерял, или говорит «Хватит» — поединок окончен.'
+				},
+				{
+					title: 'Восьмое и последнее правило Бойцовского клуба.',
+					body: 'Новичок обязан принять бой.'
+				},
 
-// 			];
-// "
+			];
+
+
+function getRules(rules){
+	for (i of rules){
+		let key = document.createElement('div');
+		key.innerText = i.title;
+		
+		let value = document.createElement('div');
+		value.innerText = i.body;
+		
+		document.body.append(key);
+    document.body.append(value);
+    document.write("---------------------------");
+	}
+}
+getRules(rules);
+
+
 
 // ===========додаткове від віктора========
 // 1) Точная степень двойки.
